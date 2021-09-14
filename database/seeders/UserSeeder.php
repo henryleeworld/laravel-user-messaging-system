977c8435d2141c351c47355c8e7ce2bd01b6d9f3
Henry Lee <henryleeworld@gmail.com>
2021-09-15 06:51:26 +0800

    feat:初始化

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+            [
+                'id'             => 1,
+                'name'           => '管理者',
+                'email'          => 'admin@admin.com',
+                'password'       => bcrypt('password'),
+                'remember_token' => null,
+            ],
+            [
+                'id'             => 2,
+                'name'           => '櫻木花道',
+                'email'          => 'sakuragi_hanamichi@slamdunk.com',
+                'password'       => bcrypt('password'),
+                'remember_token' => null,
+            ],
+            [
+                'id'             => 3,
+                'name'           => '流川楓',
+                'email'          => 'rukawa_kaede@slamdunk.com',
+                'password'       => bcrypt('password'),
+                'remember_token' => null,
+            ],
+            [
+                'id'             => 4,
+                'name'           => '赤木剛憲',
+                'email'          => 'akagi_takenori@slamdunk.com',
+                'password'       => bcrypt('password'),
+                'remember_token' => null,
+            ],
+            [
+                'id'             => 5,
+                'name'           => '宮城良田',
+                'email'          => 'miyagi_ryota@slamdunk.com',
+                'password'       => bcrypt('password'),
+                'remember_token' => null,
+            ],
+            [
+                'id'             => 6,
+                'name'           => '三井壽',
+                'email'          => 'mitsui_hisashi@slamdunk.com',
+                'password'       => bcrypt('password'),
+                'remember_token' => null,
+            ],
+            [
+                'id'             => 7,
+                'name'           => '木暮公延',
+                'email'          => 'kogure_kiminobu@slamdunk.com',
+                'password'       => bcrypt('password'),
+                'remember_token' => null,
+            ],
+        ];
+
+        User::insert($users);
+    }
+}
---
+++ resources/views/messenger/partials/no-threads.blade.php
@@ -0,0 +1,1 @@
+<span class="block w-full p-2 mb-4 text-blue-800 bg-blue-300">{{ __('You have no messages.') }}</span>
